bd199fbf25125d88a95b924a7f00b348b4620ad2
kalyani kasar <kalyanikasar@kalyanis-MacBook-Pro.local>
2023-11-19 16:56:35 +0530

    api route

--- pages/api/proDataApiRoute.js
+++ pages/api/proDataApiRoute.js
@@ -1,8 +1,5 @@
-import {proData} from '../../data/proData';
+import { proData } from "../../data/proData";
 
-
-export default function handler(req,res){
-
-
-   res.status(200).json(proData)
+export default function handler(req, res) {
+  res.status(200).json(proData);
 }
